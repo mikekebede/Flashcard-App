98752f2007a179d93f5d2848bcc0f84ba0462710
mikekebede <mikyasyesigat@gmail.com>
2022-10-02 14:08:17 -0500

    new commit

---
+++ app/src/main/java/com/example/coolflashcardapp/AddCardActivity.kt
@@ -0,0 +1,35 @@
+package com.example.coolflashcardapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+
+class AddCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_card)
+
+        val questionEditText=findViewById<EditText>(R.id.flashcard_question_editText)
+        val answerEditText=findViewById<EditText>(R.id.flashcard_answer_editText)
+
+        val saveButton=findViewById<ImageView>(R.id.flashcard_save_button)
+        saveButton.setOnClickListener{
+
+            val questionString=questionEditText.text.toString()
+            val answerString=answerEditText.text.toString()
+
+            val data= Intent()
+            data.putExtra("QUESTION_KEY", questionString)
+            data.putExtra("ANSWER_KEY", answerString)
+
+            setResult(RESULT_OK, data)
+            finish()
+        }
+        val cancelButton=findViewById<ImageView>(R.id.flashcard_cancel_button)
+        cancelButton.setOnClickListener{
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/example/coolflashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/coolflashcardapp/MainActivity.kt
@@ -1,33 +1,67 @@
 package com.example.coolflashcardapp
 
-import android.os.Build.ID
+import android.content.Intent
+//import android.os.Build.ID
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+//import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val flash_question=findViewById<TextView>(R.id.flashcard_question)
-        val flash_answer=findViewById<TextView>(R.id.flashcard_answer)
+        val flashQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashAnswer = findViewById<TextView>(R.id.flashcard_answer)
 
 
-        flash_question.setOnClickListener{
-            findViewById<View>(R.id.flashcard_answer).visibility = View.VISIBLE
-            findViewById<View>(R.id.flashcard_question).visibility=View.INVISIBLE
 
-            Toast.makeText(this, "Question button was clicked", Toast.LENGTH_SHORT)
+        flashQuestion.setOnClickListener {
+            flashAnswer.visibility = View.VISIBLE
+            flashQuestion.visibility = View.INVISIBLE
+
+            Toast.makeText(this, "Question button was clicked", Toast.LENGTH_SHORT).show()
+        }
+        flashAnswer.setOnClickListener {
+           flashQuestion.visibility = View.VISIBLE
+            flashAnswer.visibility = View.INVISIBLE
         }
 
-        flash_answer.setOnClickListener{
-            findViewById<View>(R.id.flashcard_question).visibility=View.VISIBLE
-            findViewById<View>(R.id.flashcard_answer).visibility=View.INVISIBLE
+
+       val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+            val data: Intent?=result.data
 
 
+            if(data!= null){
+                val questionString=data.getStringExtra("QUESTION_KEY")
+                val answerString=data.getStringExtra("ANSWER_KEY")
+
+                flashQuestion.text=questionString
+                flashAnswer.text=answerString
+
+                Log.i( "Mike :MainActivity", "Question : $questionString" )
+                Log.i( "Mike :MainActivity", "Question : $answerString" )
+
+            }
+            else{
+                Log.i( "Mike :MainActivity", "returned null from AddCardActivity" )
+            }
         }
+
+        val addQuestionButton=findViewById<ImageView>(R.id.add_question_button)
+        addQuestionButton.setOnClickListener{
+            val intent=Intent(this , AddCardActivity::class.java)
+            resultLauncher.launch(intent)
+
+            Log.i("Yesigat" , "it was clicked")
+        }
+
     }
 }
